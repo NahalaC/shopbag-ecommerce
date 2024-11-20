67cdbfdc11ff8e1fdcee4b7151cb42efa33c1609
NahalaC <fathimathunnahala98@gmail.com>
2024-11-20 15:15:27 +0530

    solved g-auth

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL,
-  scope: ['profile', 'email']
+  
 },async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id })
