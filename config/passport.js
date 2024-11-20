4c4e8f634340c050cd20f5a93d6e8514f3ab98fd
NahalaC <fathimathunnahala98@gmail.com>
2024-11-20 13:51:04 +0530

    solved auth

--- config/passport.js
+++ config/passport.js
@@ -7,8 +7,10 @@ require('dotenv').config()
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  callbackURL:" https://shopebag.site/auth/google/callback",
+  passReqToCallback:true
 },
+
 async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id })
